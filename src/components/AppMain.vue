b86dc5e7c8fd02697d1cd94d1cd0d5a555fce077
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-10 17:35:34 +0100

    impostazione Main

---
+++ src/components/AppMain.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    name: "AppMain"
+}
+</script>
+
+
+<template>
+    <div class="container mt-5">
+        <div class="results d-flex align-items-center">
+            <h2 class="">Found 62 characters</h2>
+        </div>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+.container {
+    background-color: white;
+    height: 300px;
+
+    .results {
+        background-color: rgb(41, 39, 39);
+        color: white;
+
+
+
+    }
+}
+</style>
