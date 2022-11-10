86bfc1891ee722936be270e076a954e1606a426b
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-10 18:00:31 +0100

    creato store e prelevato dati api

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,21 @@
 import axios from "axios";
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
+import { store } from "./store";
 export default {
   components: {
     AppHeader,
     AppMain
   },
+  data() {
+    return {
+      store
+    }
+  },
   created() {
     axios.get("https://www.breakingbadapi.com/api/characters").then((resp) => {
-      console.log(resp);
+      this.store.characters = resp.data;
+      console.log(this.store.characters);
     })
   }
 }
