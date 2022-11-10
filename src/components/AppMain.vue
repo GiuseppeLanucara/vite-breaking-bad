2046656d163fafa6e1a80f3adbaee1ec00bbce8b
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-11-10 18:39:09 +0100

    implementata grafica API chiamata personaggi

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,16 @@
 <script>
+import { store } from "../store";
+import CharacterCard from "./CharacterCard.vue";
 export default {
-    name: "AppMain"
+    name: "AppMain",
+    components: {
+        CharacterCard
+    },
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 
@@ -10,15 +20,25 @@ export default {
         <div class="results d-flex align-items-center ">
             <h2 class="">Found 62 characters</h2>
         </div>
-    </div>
+        <div class="row row-cols-2 row-cols-md-4 mt-5">
+            <div class="col" v-for="character in store.characters" :key="character.id">
+                <CharacterCard :character="character" />
+            </div>
+        </div>
 
+
+    </div>
 </template>
 
 
 <style lang="scss" scoped>
 .container {
+
     background-color: white;
-    height: 300px;
+    overflow: scroll;
+    height: 800px;
+
+
 
     .results {
         background-color: rgb(41, 39, 39);
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: "CharacterCard",
+    props: {
+        character: Object
+    }
+}
+</script>
+
+
+<template>
+    <div class="character-cart d-flex justify-content-center flex-column">
+        <img :src="character.img" alt="">
+        <h3>{{ character.name }}</h3>
+        <h4>{{ character.category }}</h4>
+        <h5>{{ character.status }}</h5>
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+.character-cart {
+    background-color: rgb(41, 39, 39);
+    text-align: center;
+    margin-bottom: 30px;
+    color: white;
+    height: 600px;
+
+    img {
+        margin-left: 30px;
+        margin-right: 30px;
+        margin-top: 20px;
+        height: 350px;
+    }
+
+    h4 {
+        color: grey;
+    }
+
+    h5 {
+        color: grey;
+        font-weight: lighter;
+    }
+}
+</style>
